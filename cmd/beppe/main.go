829bb9dab526f3d824746ce96d4f9d9c761a93b2
n <neurogas@gmail.com>
2022-04-13 10:37:36 +0200

    MAIN: Update deps and hook for services

--- cmd/beppe/main.go
+++ cmd/beppe/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
-	"github.com/ennetech/beppe/pkg/services/http"
+	"github.com/ennetech/beppe/pkg/acme"
+	"github.com/ennetech/beppe/pkg/api"
+	"github.com/ennetech/beppe/pkg/cron"
+	"github.com/ennetech/beppe/pkg/http"
+	"github.com/ennetech/beppe/pkg/storage"
 	"github.com/ennetech/go-common/env"
 	"github.com/ennetech/go-common/logz"
 	"os"
@@ -13,7 +17,18 @@ func main() {
 	logz.DebugOn = env.Get("DEBUG_ENABLED", "false") == "true"
 
 	logz.Info("BEPPE", "Starting...")
-	http.Start()
+	storage.Init(env.Get("STORAGE_DRIVER", "vault"))
+	acme.Init(env.Get("ACME_SOLVER", "route53"))
+	if env.Get("BEPPE_HTTP", "false") == "true" {
+		http.Start(env.Get("HTTP_PORT", "8080"))
+	}
+	if env.Get("BEPPE_API", "false") == "true" {
+		api.Start(env.Get("API_PORT", "4367"), env.Get("API_TOKEN", "panini"))
+	}
+	if env.Get("BEPPE_CRON", "false") == "true" {
+		cron.Init()
+	}
+
 	WaitForCtrlC()
 }
 
